7351b653f434093468d26759665533a8c9d8ec7d
danigh1234 <gherdaniel7@gmail.com>
2022-04-14 10:41:55 -0700

    positive_or_negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry to the function
+ *
+ * function differentiate b/n bin positive and negetive num
+ *
+ * returns: zero if successful
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negetive(i);
+
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+c-code debugging using intective debugging method
