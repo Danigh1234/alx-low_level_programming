d1ed68cb4208f0895513f2343f71b3e0437fcace
danigh1234 <gherdaniel7@gmail.com>
2022-04-15 17:53:13 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * program entry is main
+ *
+ * program prints single digit ints
+ *
+ * return: 0 if success
+ */
+int main(void)
+{
+char n;
+for (n ='0'; n <= '9'; n++)
+{
+putchar (n);
+}
+putchar ('\n');
+
+return (0);
+}
