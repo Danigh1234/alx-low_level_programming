3e69ac63ec788c7eabc05c05ab83a48a9458233a
danigh1234 <gherdaniel7@gmail.com>
2022-06-09 03:31:02 -0700

    2vf

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - print num give as parameter with given const string in between
+ * @separator: cost string to be printed b/n num
+ * @n: num of num to printed
+ * @...: num to printed
+ */
+int print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list num;
+va_start(num, n)
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(num, int));
+if (i = n - 1 && separator)
+printf("%s", separator);
+}
+printf("\n");
+va_end(num);
+}
