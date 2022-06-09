32ede155a261305e610d25bb13d39daa790a4c25
danigh1234 <gherdaniel7@gmail.com>
2022-06-09 05:55:18 -0700

    2vf

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ va_start(num, n);
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(num, int));
-if (i = (n - 1) && separator)
+if (i < (n - 1) && separator)
 printf("%s", separator);
 }
 printf("\n");i
